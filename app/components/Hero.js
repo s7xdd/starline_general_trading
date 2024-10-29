3231d7059fff6c9d064a530dd05e80ac8b429210
Mohammed Shamil <kmshamil123@gmail.com>
2024-10-29 13:32:18 +0530

    Updated Hero

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import React from 'react';
 import Statistics from './Statistics';
+import logo from '../../public/starline-new.png';
 
 function Hero() {
   return (
@@ -16,11 +17,12 @@ function Hero() {
         <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black"></div>
       </div>
       <div className="absolute inset-0 flex flex-col justify-center items-center text-white z-20 px-4 sm:px-10 pt-10 sm:pt-0">
-        <div className=''>
-            <h1 className="text-[100px] font-bold bg-gradient-to-r from-orange-400 via-red-500 to-pink-500 bg-clip-text text-transparent ">
+        {/* <div className='bg-white bg-opacity-20 p-3 mb-3 rounded-lg pl-10 pr-10'>
+            <h1 className="text-[50px] text-center mb-5 leading-[60px] sm:text-[100px] sm:leading-[130px] md:leading-[100px] text-5xl md:font-2xl lg:leading-1 font-bold bg-gradient-to-r from-orange-400 via-red-500 to-pink-500 bg-clip-text text-transparent ">
                 Starline General Trading LLC
             </h1>
-        </div>
+        </div> */}
+        <Image src={logo} height={250} className='mb-8'/>
         <h2 className="text-4xl sm:text-6xl md:text-6xl font-bold mt-0">#GlobalLogistics</h2>
         <p className="mt-5 max-w-xs sm:max-w-md md:max-w-xl lg:max-w-2xl text-sm sm:text-base text-center">
           We specialize in handling various aspects of logistics, including transportation, warehousing, inventory management, and distribution.
