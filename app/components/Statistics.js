fb53374632f06de128807fe74072718e2b95cb1c
Mohammed Shamil <kmshamil123@gmail.com>
2024-10-29 12:52:12 +0530

    Updated statistics font color

--- app/components/Statistics.js
+++ app/components/Statistics.js
@@ -25,22 +25,22 @@ function Statistics() {
                 <div ref={clientRef} className="flex flex-col items-center">
                     <h3 className="text-5xl sm:text-7xl md:text-8xl font-bold">
                         {clientVisible && <CountUp end={500} duration={3} />}
-                        <span className='text-orange-500'>+</span>
+                        <span className='text-red-600'>+</span>
                     </h3>
-                    <p className='text-orange-500 text-lg sm:text-xl mt-2'>Happy Clients</p>
+                    <p className='text-red-600 text-lg sm:text-xl mt-2'>Happy Clients</p>
                 </div>
                 <div ref={partnerRef} className="flex flex-col items-center">
                     <h3 className="text-5xl sm:text-7xl md:text-8xl font-bold">
                         {partnerVisible && <CountUp end={30} duration={4} suffix="+" />}
                     </h3>
-                    <p className='text-orange-500 text-lg sm:text-xl mt-2'>Partner Brands</p>
+                    <p className='text-red-600 text-lg sm:text-xl mt-2'>Partner Brands</p>
                 </div>
                 <div ref={yearsRef} className="flex flex-col items-center">
                     <h3 className="text-5xl sm:text-7xl md:text-8xl font-bold">
                         {yearsVisible && <CountUp end={20} duration={5} />}
-                        <span className='text-orange-500'>+</span>
+                        <span className='text-red-600'>+</span>
                     </h3>
-                    <p className='text-orange-500 text-lg sm:text-xl mt-2'>Years of experience</p>
+                    <p className='text-red-600 text-lg sm:text-xl mt-2'>Years of experience</p>
                 </div>
             </div>
         </div>
